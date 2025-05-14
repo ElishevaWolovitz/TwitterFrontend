ce4bdac5e0d6a7dad9930e7e292f5c9b192009b6
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-05-14 11:17:57 +0300

    tester app for read all users

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,42 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios";
+
+const baseUrl = import.meta.env.VITE_API_URL;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState<{ _id: string, username: string, displayName: string }[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const fetchUsers = () => {
+    setLoading(true);
+    setError("");
+    axios
+      .get(`${baseUrl}/users`)
+      .then((res) => {
+        setUsers(res.data);
+        setLoading(false);
+      })
+      .catch(() => {
+        setError("Failed to load users");
+        setLoading(false);
+      });
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{ padding: "2rem" }}>
+      <h1>All Items</h1>
+      <button onClick={fetchUsers}>Load users</button>
+
+      {loading && <p>Loading...</p>}
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
+      <ul>
+        {users.map((user) => (
+          <li key={user._id}>{user.username}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
