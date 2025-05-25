ac4fff0da2f32b39f3517e3c00e73e1965cecf00
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-05-25 15:15:55 +0300

    fixed display of data by accessign res.data.data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from "axios";
-import type {UserTypeFront} from './types/user.type';
+import type { UserTypeFront } from './types/user.type';
 
 
 const api = axios.create({baseURL: `http://localhost:3000`});
@@ -9,26 +9,28 @@ function App() {
 
   const fetchUsers = () => {
     api.get('/users')
-    .then((res) => { 
-      const data = Array.isArray(res.data) ? res.data : [res.data];
-      setUsers(data);
+    .then((res) => {
+      console.log("res", res)
+     // const data = Array.isArray(res.data) ? res.data : [res.data];
+
+      setUsers(res.data.data);
     });
   };
   useEffect(() => {
     console.log("users", users)
-    console.log("data", users.map((user) => user._id))
+    console.log("data", users.map((user) => {return user}))
   }, [users]);
   return (
     <div style={{ padding: "2rem" }}>
       <h1>All Users</h1>
       <button onClick={fetchUsers}>Load users</button>
       <ul>
-        {/* {users.map((user, index) => (
+        {users.map((user, index) => (
           <li key={user._id || index}>
             <h2>{user.username}</h2>
             <p>{user.displayName}</p>
           </li>
-        ))} */}
+        ))}
       </ul>
     </div>
   )
