17bc4d676f8ed6613c3ccb9562fae638c55ee583
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-05-26 16:07:37 +0300

    set up basic routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,17 @@
-import { useState, useEffect } from 'react';
-import axios from "axios";
-import type { ShmoozerType } from './types/shmoozer.type';
+import {Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import KibsPage from './pages/KibsPage';
+import ShmoozersPage from './pages/ShmoozersPage';
 
-
-const api = axios.create({baseURL: `http://localhost:3000`});
 function App() {
-  const [shmoozers, setUsers] = useState<ShmoozerType[]>([]);
-
-  const fetchUsers = () => {
-    api.get('/shmoozers')
-    .then((res) => {
-      console.log("res", res)
-      setUsers(res.data.data);
-    });
-  };
-  useEffect(() => {
-    console.log("shmoozers", shmoozers)
-    console.log("data", shmoozers.map((shmoozer) => {return shmoozer}))
-  }, [shmoozers]);
   return (
-    <div style={{ padding: "2rem" }}>
-      <h1>All Users</h1>
-      <button onClick={fetchUsers}>Load shmoozers</button>
-      <ul>
-        {shmoozers.map((shmoozer, index) => (
-          <li key={shmoozer._id || index}>
-            <h2>{shmoozer.shmoozerName}</h2>
-            <p>{shmoozer.displayName}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+    <Routes>
+      <Route path="/" element={<HomePage/>} />
+      <Route path="kibs" element={<KibsPage/>} />
+      <Route path="shmoozers" element={<ShmoozersPage/>} />
+    </Routes>
+    </>
   )
 }
 
