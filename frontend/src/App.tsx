fcfb616f77886f19e76276f2ddb97b05caaf485c
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-05-26 16:24:37 +0300

    app page routes created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,16 @@
-import {Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage/index.ts';
 import KibsPage from './pages/KibsPage';
 import ShmoozersPage from './pages/ShmoozersPage';
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route path="/" element={<HomePage/>} />
-      <Route path="kibs" element={<KibsPage/>} />
-      <Route path="shmoozers" element={<ShmoozersPage/>} />
-    </Routes>
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='kibs' element={<KibsPage/>} />
+        <Route path='shmoozers' element={<ShmoozersPage/>} />
+      </Routes>
     </>
   )
 }
