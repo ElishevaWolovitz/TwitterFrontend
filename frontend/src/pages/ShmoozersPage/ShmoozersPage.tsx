b250874179ea53dc88ea64a4433b9f29cfff1124
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-05-27 16:47:15 +0300

    testing basic list

--- frontend/src/pages/ShmoozersPage/ShmoozersPage.tsx
+++ frontend/src/pages/ShmoozersPage/ShmoozersPage.tsx
@@ -1,10 +1,17 @@
 import Navbar from '../../components/Navbar';
+import BasicList from '../../components/List/BasicList';
 
+const mockData = [ 'apple', 'banana', 'orange', 'grape', 'kiwi', 'mango', 'peach', 'pear', 'pineapple', 'strawberry' ];
+const mockDataFunc = (item: string) => {return `${item}`;}
 const ShmoozersPage = () => {
   return (
     <>
       <Navbar />
-      <h1>Shmoozer Page</h1>
+      <h1>Shmoozers Page</h1>
+      <BasicList 
+        items={mockData}
+        printItems={mockDataFunc}
+        />
 
     </>
   )
