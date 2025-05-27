626c597b228def7dbb404f65c57d863c5e199f73
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-05-27 11:11:14 +0300

    test BasicList

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,12 @@
-
+import BasicList from '../../components/List/BasicList'
 
 const HomePage = () => {
   return (
     <>
         <h1>Home Page</h1>
+        <BasicList
+          items={['apple', 'orange', 'pear']}
+          printItems={(item) => <span>{item}</span>} />
     </>
   )
 }
