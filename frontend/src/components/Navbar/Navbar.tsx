ccced174a4a2ab47d94ab001efa7ec4d9e1c9677
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-05-27 13:41:33 +0300

    implemented Navbar basics

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,10 +1,21 @@
+import { NavLink } from "react-router-dom";
+import { navItems } from "./const";
 
 
 const Navbar = () => {
-  return (
-    <>
-    </>
-  )
-}
+    return (
+    <nav>
+        <ul>
+        {navItems.map((item) => (
+            <li key={item.to}>
+                <NavLink to={item.to}>
+                    {item.label}
+                </NavLink>
+            </li>
+        ))}
+        </ul>
+    </nav>
+    );
+};
 
-export { Navbar }
+export { Navbar };
