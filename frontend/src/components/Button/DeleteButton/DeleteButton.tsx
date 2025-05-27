69e45845d37a8c880f5b77e6ed5577a23981a120
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-05-27 15:47:26 +0300

    untested logic implemented for delete button

--- frontend/src/components/Button/DeleteButton/DeleteButton.tsx
+++ frontend/src/components/Button/DeleteButton/DeleteButton.tsx
@@ -1,7 +1,10 @@
-const DeleteButton =() => {
+import type { DeleteButtonProps } from "./types";
+
+const DeleteButton =({onClick}: DeleteButtonProps) => {
     return (
-        <>
-        </>
+        <button type="button" onClick={onClick} aria-label="Delete">
+            Delete
+        </button>
     )
 }
 
