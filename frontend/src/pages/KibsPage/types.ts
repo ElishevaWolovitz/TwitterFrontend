9b270ea2fc86f0684d291b43c094839a27561a09
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-05-28 10:35:00 +0300

    created types for kibs and shmoozers

---
+++ frontend/src/pages/KibsPage/types.ts
@@ -0,0 +1,9 @@
+export type KibType = {
+  _id?: string;
+  kibName: string;
+  shmoozerId: string;
+  text: string;
+  media?: string;
+  likes?: number;
+  createdAt?: Date;
+};
---
+++ frontend/src/pages/ShmoozersPage/types.ts
@@ -0,0 +1,5 @@
+export type ShmoozerType = {
+  _id?: string;
+  shmoozerName: string;
+  displayName: string;
+};
