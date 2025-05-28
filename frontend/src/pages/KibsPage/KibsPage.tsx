379733eeea87cb67d6ca69b4fd565abf92aaab48
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-05-28 10:34:10 +0300

    messy kibs page set up with mock data

--- frontend/src/pages/KibsPage/KibsPage.tsx
+++ frontend/src/pages/KibsPage/KibsPage.tsx
@@ -1,10 +1,28 @@
-import Navbar from '../../components/Navbar'
+import Navbar from '../../components/Navbar';
+import List from '../../components/List';
+
+
+const mockKibs = [ 'apple', 'banana', 'orange', 'grape', 'kiwi', 'mango', 'peach', 'pear', 'pineapple', 'strawberry' ];
+const mockKibFunc = (item: string) => {return `${item}`;}
+
 
 const KibsPage = () => {
+  const handleEdit = (item: string) => {
+    console.log(`Edit item: ${item}`); 
+  }
+  const handleDelete = (item: string) => {
+    console.log(`Delete item: ${item}`);
+  }; 
   return (
     <>
       <Navbar />
       <h1>Kibs Page</h1>
+      <List 
+        items={mockKibs}
+        printItems={mockKibFunc}
+        onEdit={handleEdit}
+        onDelete={handleDelete}
+        />
     </>
   )
 }
