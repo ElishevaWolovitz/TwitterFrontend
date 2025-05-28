8ad545387188231efef748a0001e9dd36e02275f
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-05-28 13:05:25 +0300

    moved mockdata and functions to seperate files

--- frontend/src/pages/KibsPage/KibsPage.tsx
+++ frontend/src/pages/KibsPage/KibsPage.tsx
@@ -1,27 +1,19 @@
 import Navbar from '../../components/Navbar';
 import List from '../../components/List';
-
-
-const mockKibs = [ 'apple', 'banana', 'orange', 'grape', 'kiwi', 'mango', 'peach', 'pear', 'pineapple', 'strawberry' ];
-const mockKibFunc = (item: string) => {return `${item}`;}
+import { printKib, editKib, deleteKib } from './functions';
+import { mockKibs } from './mockKibs';
 
 
 const KibsPage = () => {
-  const handleEdit = (item: string) => {
-    console.log(`Edit item: ${item}`); 
-  }
-  const handleDelete = (item: string) => {
-    console.log(`Delete item: ${item}`);
-  }; 
   return (
     <>
       <Navbar />
       <h1>Kibs Page</h1>
       <List 
         items={mockKibs}
-        printItems={mockKibFunc}
-        onEdit={handleEdit}
-        onDelete={handleDelete}
+        printItems={printKib}
+        onEdit={editKib}
+        onDelete={deleteKib}
         />
     </>
   )
