c1fdbf8e46249ce49614a233b1a1dc120afac3da
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-05-29 16:09:52 +0300

    created axios call and passed as param to shmooze page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,11 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import KibsPage from './pages/KibsPage';
 import ShmoozersPage from './pages/ShmoozersPage';
+import axios from "axios"; 
+
+const baseURL = import.meta.env.VITE_API_BASE_URL; 
+const api = axios.create({ baseURL });             
+
 
 function App() {
   return (
@@ -9,7 +14,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage/>} />
         <Route path='kibs' element={<KibsPage/>} />
-        <Route path='shmoozers' element={<ShmoozersPage/>} />
+        <Route path='shmoozers' element={<ShmoozersPage api={api}/>} />
       </Routes>
     </>
   )
