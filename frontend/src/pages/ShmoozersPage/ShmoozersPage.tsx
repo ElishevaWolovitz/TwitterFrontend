725b33f4f897f332352c5c9f93478bab40c41344
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-05-29 16:19:44 +0300

    added fetch shmoozers logic

--- frontend/src/pages/ShmoozersPage/ShmoozersPage.tsx
+++ frontend/src/pages/ShmoozersPage/ShmoozersPage.tsx
@@ -1,18 +1,30 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
+import type { AxiosInstance } from "axios";
 import Navbar from '../../components/Navbar';
 import List from '../../components/List';
-import { mockShmoozers } from './mockShmoozers';
 import { printShmoozer } from './functions';
-import type { ShmoozerPageProps } from './types';
+import type { ShmoozerPageProps, ShmoozerType } from './types';
 
-//Q:Did i have to make type for ShmoozerPageProp here instead of the api just being of type string? 
+const [shmoozers, setUsers] = useState<ShmoozerType[]>([]);
+
+const fetchUsers = (api: AxiosInstance) => {
+  api.get('/shmoozers')
+  .then((res) => {
+    console.log("res", res)
+    setUsers(res.data.data);
+  });
+};
+
+//Q:why did i have to make type for ShmoozerPageProp here instead of the api just being of type AxiosInstance? 
+// (when i did it was an error in the App where i put shmoozerpage component)
 const ShmoozersPage = ({api}: ShmoozerPageProps) => {
+  fetchUsers(api);
   return (
     <>
       <Navbar />
       <h1>Shmoozers Page</h1>
       <List 
-        items={mockShmoozers}
+        items={shmoozers}
         printItem={printShmoozer}
         />
 
