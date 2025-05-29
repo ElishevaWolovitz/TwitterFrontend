5b980336a0521a94029d00cd2e1110549231b418
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-05-29 16:19:20 +0300

    added breacklines to printing of shmoozer

--- frontend/src/pages/ShmoozersPage/functions.ts
+++ frontend/src/pages/ShmoozersPage/functions.ts
@@ -1,8 +1,8 @@
 import type { ShmoozerType } from "./types";
 
 export const printShmoozer = (shmoozer: ShmoozerType): string => {
-    const printable = `id: ${shmoozer._id},
-    shmoozer name: ${shmoozer.shmoozerName},
+    const printable = `id: ${shmoozer._id},<br/>
+    shmoozer name: ${shmoozer.shmoozerName},<br/>
     display name: ${shmoozer.displayName}`
     return printable; 
 }
