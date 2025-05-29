e06d0239e07c6281ad2de3109134da837e599cd0
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-05-29 16:41:09 +0300

    started working on getting data from backend but not working yet

--- frontend/src/pages/ShmoozersPage/ShmoozersPage.tsx
+++ frontend/src/pages/ShmoozersPage/ShmoozersPage.tsx
@@ -1,24 +1,24 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import type { AxiosInstance } from "axios";
 import Navbar from '../../components/Navbar';
 import List from '../../components/List';
 import { printShmoozer } from './functions';
 import type { ShmoozerPageProps, ShmoozerType } from './types';
 
-const [shmoozers, setUsers] = useState<ShmoozerType[]>([]);
 
 const fetchUsers = (api: AxiosInstance) => {
   api.get('/shmoozers')
-  .then((res) => {
-    console.log("res", res)
-    setUsers(res.data.data);
-  });
 };
 
 //Q:why did i have to make type for ShmoozerPageProp here instead of the api just being of type AxiosInstance? 
 // (when i did it was an error in the App where i put shmoozerpage component)
 const ShmoozersPage = ({api}: ShmoozerPageProps) => {
+  const [shmoozers, setUsers] = useState<ShmoozerType[]>([]);
   fetchUsers(api);
+  useEffect(() => {
+    console.log("shmoozers", shmoozers)
+    console.log("data", shmoozers.map((shmoozer) => {return shmoozer}))
+  }, [shmoozers]);
   return (
     <>
       <Navbar />
