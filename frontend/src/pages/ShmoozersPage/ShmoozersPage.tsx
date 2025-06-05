9d9f56af74b7696326d307ab6029df45b30ac4c2
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-06-05 13:48:44 +0300

    updated to get data from backend instead of mock data

--- frontend/src/pages/ShmoozersPage/ShmoozersPage.tsx
+++ frontend/src/pages/ShmoozersPage/ShmoozersPage.tsx
@@ -1,24 +1,20 @@
 import { useState, useEffect } from 'react';
-import type { AxiosInstance } from "axios";
 import Navbar from '../../components/Navbar';
 import List from '../../components/List';
 import { printShmoozer } from './functions';
 import type { ShmoozerPageProps, ShmoozerType } from './types';
 
 
-const fetchUsers = (api: AxiosInstance) => {
-  api.get('/shmoozers')
-};
-
 //Q:why did i have to make type for ShmoozerPageProp here instead of the api just being of type AxiosInstance? 
 // (when i did it was an error in the App where i put shmoozerpage component)
-const ShmoozersPage = ({api}: ShmoozerPageProps) => {
-  const [shmoozers, setUsers] = useState<ShmoozerType[]>([]);
-  fetchUsers(api);
+const ShmoozersPage = ({ api }: ShmoozerPageProps) => {
+  const [shmoozers, setShmoozers] = useState<ShmoozerType[]>([]);
+  //change this .then and no unnamed functions
   useEffect(() => {
-    console.log("shmoozers", shmoozers)
-    console.log("data", shmoozers.map((shmoozer) => {return shmoozer}))
-  }, [shmoozers]);
+    api.get('/shmoozers').then((res) => {
+      setShmoozers(res.data.data);
+    });
+  }, [api]); 
   return (
     <>
       <Navbar />
