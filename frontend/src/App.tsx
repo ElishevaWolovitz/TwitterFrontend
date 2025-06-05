f7297a8940cbc9601f28585fe3d4ecc5b31f0490
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-06-05 13:48:22 +0300

    moved declaration of api to frontend global file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import KibsPage from './pages/KibsPage';
 import ShmoozersPage from './pages/ShmoozersPage';
-import axios from "axios"; 
-
-const baseURL = import.meta.env.VITE_API_BASE_URL; 
-const api = axios.create({ baseURL });             
+import { api } from './const';           
 
 
 function App() {
