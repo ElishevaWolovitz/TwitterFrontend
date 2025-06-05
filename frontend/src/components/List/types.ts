b2f3f99c41bcbbbf8e96af0ad16458a25df1a36f
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-06-05 15:35:19 +0300

    changed type for items in listprops to items that have to have an _id

--- frontend/src/components/List/types.ts
+++ frontend/src/components/List/types.ts
@@ -1,7 +1,7 @@
 import type { ReactNode } from "react";
 
 export type ListProps<T> = {
-  items: T[];
+  items: Array<T & { _id: string }>;
   printItem: (item: T) => ReactNode;
   editItem?: (item: T) => void;
   deleteItem?: (item: T) => void;
