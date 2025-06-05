5fcd5b7583a3e567cb472312ce20f8b578f41955
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-06-05 15:36:12 +0300

    changed return type for filteritems function in searchbarprops to array of items that have to have an _id

--- frontend/src/components/SearchBar/types.ts
+++ frontend/src/components/SearchBar/types.ts
@@ -2,7 +2,7 @@ import type { ReactNode } from "react";
 
 export type SearchBarProps<T> = {
     items: T[];
-    filterItems: (items: T[], query: string) => T[];
+    filterItems: (items: T[], query: string) => Array<T & { _id: string }>;
     printItem: (item: T) => ReactNode;
     editItem?: (item: T) => void;
     deleteItem?: (item: T) => void;
