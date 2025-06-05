a907bc1c64808f4414fdda65e35fcbe9a139d3d8
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-06-05 15:36:59 +0300

    changed createListItem to only take items with types that an _id instead of just T and made each item in list have unique key

--- frontend/src/components/List/List.tsx
+++ frontend/src/components/List/List.tsx
@@ -4,8 +4,8 @@ import { map } from "lodash/fp";
 
 
 const List = <T,>({items, printItem, editItem, deleteItem}: ListProps<T>) => {
-    const createListItem = (item: T) => {
-        return <li>
+    const createListItem = (item: T & { _id: string }) => {
+        return <li key={item._id ?? JSON.stringify(item)}>
                     {printItem(item)}
                     {editItem && (<button type="button" onClick={() => editItem(item)}>Edit</button>)}
                     {deleteItem && (<button type="button" onClick={() => deleteItem(item)}>Delete</button>)}
