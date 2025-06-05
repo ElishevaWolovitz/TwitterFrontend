f1d18ff4aebefb5df71cfb519decea8c3e93882b
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-06-05 14:37:36 +0300

    changed return type of printItem function for searchbar to reactnode to accept return type of html

--- frontend/src/components/SearchBar/types.ts
+++ frontend/src/components/SearchBar/types.ts
@@ -1,7 +1,9 @@
+import type { ReactNode } from "react";
+
 export type SearchBarProps<T> = {
     items: T[];
     filterItems: (items: T[], query: string) => T[];
-    printItem: (item: T) => string;
+    printItem: (item: T) => ReactNode;
     editItem?: (item: T) => void;
     deleteItem?: (item: T) => void;
 }
