91a01c944f4a085a294f23b3792e834b64f5d891
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-06-08 15:44:02 +0300

    messy edit modal implemented to list

--- frontend/src/components/List/List.tsx
+++ frontend/src/components/List/List.tsx
@@ -1,21 +1,25 @@
 import type { ListProps } from "./types";
 import { map } from "lodash/fp";
+import { useState } from "react";
+import EditModal from '../../components/EditModal';
 
 
 
 const List = <T,>({items, printItem, editItem, deleteItem}: ListProps<T>) => {
+    const [openEditModal, setOpenEditModal] = useState(false);
     const createListItem = (item: T & { _id: string }) => {
         return <li key={item._id ?? JSON.stringify(item)}>
                     {printItem(item)}
-                    {editItem && (<button type="button" onClick={() => editItem(item)}>Edit</button>)}
+                    {editItem && (<button type="button" onClick={() => setOpenEditModal(true)}>Edit</button>)}
                     {deleteItem && (<button type="button" onClick={() => deleteItem(item)}>Delete</button>)}
                 </li>
     }
     return (
         <>
-        <ul>
-            {map(createListItem)(items)}
-        </ul>
+            <ul>
+                {map(createListItem)(items)}
+            </ul>
+            {openEditModal && <EditModal openModal={openEditModal} setOpenModal={setOpenEditModal} />}
         </>
     )
 }
