bd06350599ad678a45c692d09070c816883f6f7b
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-06-09 11:06:14 +0300

    changed prop for KibEditModal to include an onClose function

--- frontend/src/ModalStructures/KibEditModal/types.ts
+++ frontend/src/ModalStructures/KibEditModal/types.ts
@@ -1,3 +1,14 @@
+export type KibEditModalProp = { 
+    _id?: string;
+    kibName: string;
+    shmoozerId: string; 
+    text: string;
+    media?: string;
+    likes?: number;
+    createdAt?: Date;
+    onClose: () => void;
+}
+
 export type KibType = { 
     _id?: string;
     kibName: string;
