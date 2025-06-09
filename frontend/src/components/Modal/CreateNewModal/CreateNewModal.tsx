c7881f4a0ac4b043651c5e215bdf3c4566f15551
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-06-09 16:44:48 +0300

    set up create new modal component basics

--- frontend/src/components/Modal/CreateNewModal/CreateNewModal.tsx
+++ frontend/src/components/Modal/CreateNewModal/CreateNewModal.tsx
@@ -1,6 +1,14 @@
-const CreateNewModal =() => {
+import type { CreateNewModalProps } from "./types"
+
+const CreateNewModal =<T extends object>({openModal, setOpenModal, createNewItem, children: ChildComponent, itemToCreateNew}: CreateNewModalProps<T>) => {
     return (
         <>
+            {openModal && (
+                <div>
+                    <ChildComponent {...itemToCreateNew} onClose={()=> setOpenModal(false)} createNewItem={createNewItem}/>
+                    <button type="button" onClick={() => setOpenModal(false)}>Cancel</button>
+                </div>
+            )}
         </>
     )
 }
---
+++ frontend/src/components/Modal/CreateNewModal/types.ts
@@ -0,0 +1,7 @@
+export type CreateNewModalProps<T> = {
+    openModal: boolean;
+    setOpenModal: (open: boolean) => void;
+    createNewItem: (item: T) => void;
+    children: React.ComponentType<T & { onClose: () => void } & { createNewItem: (item: T) => void }>;
+    itemToCreateNew:T;
+}
