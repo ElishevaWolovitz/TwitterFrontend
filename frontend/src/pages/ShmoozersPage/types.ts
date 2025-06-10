fbe580d004a01744a27f67e7d52d8a673e702a7a
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-06-10 11:10:52 +0300

    took away specific shmoozertype so everywhere uses same global shmoozertype

--- frontend/src/pages/ShmoozersPage/types.ts
+++ frontend/src/pages/ShmoozersPage/types.ts
@@ -3,9 +3,3 @@ import type { AxiosInstance } from "axios";
 export type ShmoozerPageProps = {
   api: AxiosInstance;
 };
-
-export type ShmoozerType = {
-  _id: string;
-  shmoozerName: string;
-  displayName: string;
-};
