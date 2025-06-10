6adeca8de3bf7b22cc7e4ea2809604287a09d154
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-06-10 10:36:12 +0300

    created logic for shmoozer create new modal

--- frontend/src/ModalStructures/ShmoozerCreateNewModal/ShmoozerCreateNewModal.tsx
+++ frontend/src/ModalStructures/ShmoozerCreateNewModal/ShmoozerCreateNewModal.tsx
@@ -1,8 +1,44 @@
-const ShmoozerCreateNewModal = () => {
-    return (
-        <>
-        </>
-    )
+import type { ShmoozerCreateNewModalProps } from "./types"
+import { useForm } from "react-hook-form"
+import type { ShmoozerType } from '../../types/shmoozer.type';
+
+const ShmoozerCreateNewModal =({onClose, createNewShmoozer}: ShmoozerCreateNewModalProps) => {
+    const {
+            register,
+            handleSubmit,             
+            formState: { errors } 
+        } = useForm<ShmoozerType>();
+
+        const onSubmit = (newShmoozer: ShmoozerType) => {
+            console.log("Submitting new shmoozer:", newShmoozer);
+            createNewShmoozer(newShmoozer);
+            onClose();
+        };
+    
+        return (
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <h3> Create New Shmoozer </h3>
+                <label>
+                    Shmoozer Name:
+                    <input
+                        type="text"
+                        {...register("shmoozerName", { required: true })}
+                        placeholder="Shmoozer Name"
+                    />
+                    {errors.shmoozerName && <span>This field is required</span>}
+                </label>
+                <label>
+                    Display Name:
+                    <input
+                        type="text"
+                        {...register("displayName", { required: true })}
+                        placeholder="Display Name"
+                    />
+                    {errors.displayName && <span>This field is required</span>}
+                </label>
+                <button type="submit">Submit</button>
+            </form>
+        )
 }
 
-export { ShmoozerCreateNewModal}
+export { ShmoozerCreateNewModal }
--- frontend/src/ModalStructures/ShmoozerCreateNewModal/types.ts
+++ frontend/src/ModalStructures/ShmoozerCreateNewModal/types.ts
@@ -1,3 +1,6 @@
+import type { ShmoozerType } from '../../types/shmoozer.type';
+
 export type ShmoozerCreateNewModalProps = {
-    
+    onClose: () => void;
+    createNewShmoozer: (item: ShmoozerType) => void;
 }
