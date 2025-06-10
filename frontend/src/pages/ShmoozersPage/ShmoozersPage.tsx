baaafec0377771f6261966c91d29513b0f3f4276
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-06-10 16:38:10 +0300

    got rid of comments

--- frontend/src/pages/ShmoozersPage/ShmoozersPage.tsx
+++ frontend/src/pages/ShmoozersPage/ShmoozersPage.tsx
@@ -9,11 +9,8 @@ import CreateNewModal from '../../components/Modal/CreateNewModal';
 import ShmoozerCreateNewModal from '../../structures/Modals/ShmoozerCreateNewModal';
 
 
-//Q:why did i have to make type for ShmoozerPageProp here instead of the api just being of type AxiosInstance? 
-// (when i did it was an error in the App where i put shmoozerpage component)
 const ShmoozersPage = ({ api }: ShmoozerPageProps) => {
   const [shmoozers, setShmoozers] = useState<ShmoozerType[]>([]);
-  //change this .then and no unnamed functions
   useEffect(() => {
     api.get('/shmoozers').then((res) => {
         setShmoozers(res.data.data)
