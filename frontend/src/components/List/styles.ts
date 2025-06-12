c7781b8b1435502f265669f6082502e7c4fb1a45
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-06-12 15:08:16 +0300

    list styles

---
+++ frontend/src/components/List/styles.ts
@@ -0,0 +1,22 @@
+import { createUseStyles } from 'react-jss';
+
+export const useListStyles = createUseStyles({
+  list: {
+    listStyle: 'none',
+    padding: 0,
+    margin: 0,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    gap: 24, // space between cards
+    width: '100%',
+    maxWidth: 600,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  listItem: {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+  },
+});
